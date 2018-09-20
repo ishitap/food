4fb4cbf4abaa08bfd331c3552aa9bae75319fe19
karshan <karshan.sharma@gmail.com>
2018-09-19 17:17:25 -0700

    deployscript use assets

--- deploy-scripts/deploy.sh
+++ deploy-scripts/deploy.sh
@@ -6,11 +6,9 @@ for f in $BASEDIR/*.md; do
     echo -n "<li><a href=\"$(basename $f | cut -d . -f 1).html\">" >> index.html
     cat tmp.html | pup 'h1 text{}' | head -n 1 | tr -d '\n' >> index.html
     echo "</a></li>" >> index.html
-    cat styleheader.html tmp.html > $BASEDIR/html/$(echo $(basename $f) | cut -d . -f 1 -s).html
+    cat $BASEDIR/deploy-scripts/styleheader.html tmp.html > $BASEDIR/html/$(echo $(basename $f) | cut -d . -f 1 -s).html
     rm tmp.html
 done
-cp $BASEDIR/styles.css $BASEDIR/html/
-cp $BASEDIR/indexstyles.css $BASEDIR/html/
+cp -R $BASEDIR/assets/* $BASEDIR/html/
 cat index.html $BASEDIR/indexfooter.html > $BASEDIR/html/index.html
 rm index.html
-cp -R $BASEDIR/gifs $BASEDIR/html/
